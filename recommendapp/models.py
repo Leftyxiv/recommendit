329ddd6b66663e4ac9e72da95a8b96001e292423
Leftyxiv <manny@aledoux.net>
2021-05-12 14:27:26 -0400

    I think the models are done for now

--- recommendapp/models.py
+++ recommendapp/models.py
@@ -12,3 +12,20 @@ class Profile(models.Model):
     friends = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Post(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    website = models.CharField(max_length=250)
+    photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    rating = models.IntegerField(min=0, max=5)
+    like = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag_words = models.CharField(max_length=100)
+    description = models.TextField()
+    comment = models.TextField()
+    # agree
+    # disagree
+    # category
+
+class Comments(models.Model):
+    poster = models.OneToOneField(User, on_delete=models.CASCADE)
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+    text = models.TextField()
