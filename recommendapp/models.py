7d457073a8c52f134972d5407399a1d0f3d4978f
Leftyxiv <manny@aledoux.net>
2021-05-12 14:37:55 -0400

    got the models in and added to site admin

--- recommendapp/models.py
+++ recommendapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 User = settings.AUTH_USER_MODEL
@@ -8,15 +9,18 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.CharField(max_length=75)
     rating_system = models.CharField(max_length=15)
-    photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
-    friends = models.ForeignKey(User, on_delete=models.CASCADE)
+    # photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    # friends = models.ForeignKey(, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
 class Post(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField()
     website = models.CharField(max_length=250)
-    photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
-    rating = models.IntegerField(min=0, max=5)
+    # photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     like = models.ForeignKey(User, on_delete=models.CASCADE)
     tag_words = models.CharField(max_length=100)
     description = models.TextField()
@@ -25,7 +29,13 @@ class Post(models.Model):
     # disagree
     # category
 
+    def __str__(self):
+        return self.name
+
 class Comments(models.Model):
     poster = models.OneToOneField(User, on_delete=models.CASCADE)
     post = models.OneToOneField(Post, on_delete=models.CASCADE)
     text = models.TextField()
+
+    def __str__(self):
+        return f"Post on {self.post} by {self.poster}"
