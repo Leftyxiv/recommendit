bf347b0793293757bf01b1440bc283efaf9daab4
Leftyxiv <manny@aledoux.net>
2021-05-12 14:13:42 -0400

    added profile model

--- recommendapp/models.py
+++ recommendapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.CharField(max_length=75)
+    rating_system = models.CharField(max_length=15)
+    photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    friends = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Post(models.Model):
